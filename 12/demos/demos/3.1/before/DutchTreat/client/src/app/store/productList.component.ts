cbaf79f3f834d4ab73f737b6a329300e8a288265
Shenyi <baoshenyi@gmail.com>
2021-08-15 15:33:27 -0600

    init

---
+++ 12/demos/demos/3.1/before/DutchTreat/client/src/app/store/productList.component.ts
@@ -0,0 +1,20 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+  selector: "product-list",
+  templateUrl: "./productList.component.html"
+})
+export class ProductList {
+
+  constructor() {
+    console.log("In Product List");
+  }
+
+  public products = [{
+    title: "Some Product",
+    price: 19.99
+  }, {
+    title: "Another Product",
+    price: 14.99
+  }];
+}
---
+++ 13/demos/demos/3.1/before/DutchTreat/client/src/app/views/productListView.ts
@@ -0,0 +1,19 @@
+﻿import { Component, OnInit } from "@angular/core";
+import Store from "../store";
+
+@Component({
+  selector: "product-list",
+  templateUrl: "./productListView.html",
+  styleUrls: ["./productListView.css"]
+})
+export class ProductListView implements OnInit {
+
+  constructor(public store: Store) {
+  }
+
+  async ngOnInit() {
+    this.store.loadProducts();
+  }
+
+
+}
